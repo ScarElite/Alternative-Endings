00908e36b68ae13ed4d9dfda9cba2ae3d15776c3
Mitchell Armstrong <mra0211@gmail.com>
2022-11-01 22:35:47 -0400

    Added a dashboard-routes file that renders the dashboard handlebars template that was also created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,7 @@
-const router = require("express").Router();
+// const router = require("express").Router();
+
+// const userRoutes = require("./user-routes.js");
+
+// router.use("/users", userRoutes);
+
+// module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const homeRoutes = require("./home-routes");
 const dashboardRoutes = require("./dashboard-routes");
 
 router.use("/", homeRoutes);
-// router.use("/dashboard", dashboardRoutes);
+router.use("/dashboard", dashboardRoutes);
 // router.use("/api", apiRoutes);
 
 module.exports = router;
