bf08c2bba58064fd4b1611706b45a097d8ec4846
Mitchell Armstrong <mra0211@gmail.com>
2022-11-03 21:50:32 -0400

    Pulled the new stuff from the develop branch

--- public/javascript/movie.js
+++
@@ -1,7 +0,0 @@
-async function movieModal(event) {
-  event.preventDefault();
-
-  console.log("Movie Clicked!!!!!");
-}
-
-document.querySelector(".upcoming-movie").addEventListener("click", movieModal);
