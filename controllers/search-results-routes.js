45379de6b2431ab5636bbd09f4abebafc5d44e01
Mitchell Armstrong <mra0211@gmail.com>
2022-11-05 15:22:22 -0400

    Added the correct headers to the GET request when searching for a movie

--- controllers/search-results-routes.js
+++ controllers/search-results-routes.js
@@ -3,32 +3,26 @@ const sequelize = require("../config/connection");
 
 router.get("/", (req, res) => {
   console.log("+++++++++++++++++++++++++++++");
-  //   res.render("search-results");
+
+  // const apiUrl =
+  //   "https://api.themoviedb.org/3/movie/?&apiKey=" +
+  //   process.env.API_KEY_1 +
+  //   "&query=hello";
+
+  // fetch(apiUrl)
+  //   .then((data) => {
+  //     console.log("MOVIE", data.body);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
+
+  res.render("search-results");
 });
 
 router.get("/:title", (req, res) => {
   console.log("+++++++++++++++++++++++++++++");
-
-  var userText = document.querySelector(".input");
-  var input = userText.value.trim();
-
-  const apiUrl =
-    "https://api.themoviedb.org/3/movie/" +
-    input +
-    "&apiKey=" +
-    process.env.API_KEY_1;
-
-  console.log(input);
-
-  fetch(apiUrl)
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-
   res.render("search-results");
 });
 
--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,18 +1,44 @@
-async function searchResults(event) {
+function searchResults(event) {
   event.preventDefault();
 
-  const response = await fetch(`/results`, {
+  var userText = document.querySelector(".input");
+  var input = userText.value.trim();
+  console.log(input);
+
+  const options = {
     method: "GET",
     headers: {
-      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.API_KEY_V4,
+      "Content-Type": "application/json;charset=utf-8",
     },
-  });
+  };
 
-  if (response.ok) {
-    document.location.replace("/results");
-  } else {
-    alert(response.statusText);
-  }
+  const apiUrl =
+    "https://api.themoviedb.org/3/movie/?&apiKey=" +
+    process.env.API_KEY +
+    "&query=hello";
+
+  fetch(apiUrl, options)
+    .then((data) => {
+      console.log("MOVIE", data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+  // const response = await fetch(`/results`, {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+
+  // if (response.ok) {
+  //   document.location.replace("/results");
+  // } else {
+  //   alert(response.statusText);
+  // }
 }
 
 document
