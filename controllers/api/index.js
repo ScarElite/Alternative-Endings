4a3c13766b40352a90e1315e044ab298dbafc353
Your Name <you@example.com>
2022-11-05 21:22:53 -0400

    created dashboard

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes");
-const dashboardRoutes = require("./dashboard-routes");
+const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
+const commentRoutes = require("./comment-routes");
 
-router.use("/api", apiRoutes);
-router.use("/", homeRoutes);
-router.use("/dashboard", dashboardRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
