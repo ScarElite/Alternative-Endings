7a442d5859c5d83a74fa11bb5a5a3643da24dd17
Your Name <you@example.com>
2022-11-05 21:33:30 -0400

    created user models

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require("./utils/helpers");
+// const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
-app.engine("handlebars", hbs.engine);
+// app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
