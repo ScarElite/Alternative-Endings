0af0b64fea1ef58dcd672f9961bc05d299c34092
Mitchell Armstrong <mra0211@gmail.com>
2022-11-14 11:30:59 -0500

    Took out a test route in the movie-data.js file and added some comments in that file and the scripts.jsfile

--- controllers/movie-data.js
+++ controllers/movie-data.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+// GET upcoming movie data
 router.get("/upcoming", (req, res) => {
   const apiUrl =
     "https://api.themoviedb.org/3/movie/upcoming?api_key=" +
@@ -18,24 +19,7 @@ router.get("/upcoming", (req, res) => {
     });
 });
 
-router.get("/something", (req, res) => {
-  const apiUrl =
-    "https://api.themoviedb.org/3/movie/popular?api_key=" +
-    process.env.API_KEY +
-    "&language=en-US&page=1";
-
-  fetch(apiUrl)
-    .then(function (response) {
-      response.json().then(function (data) {
-        console.log(data);
-        res.send(data);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
+// GET movies in theaters
 router.get("/theaters", (req, res) => {
   const apiUrl =
     "https://api.themoviedb.org/3/movie/now_playing?api_key=" +
