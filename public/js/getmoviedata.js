b504df39d91d42752217e04e69f9d3fe0d069e43
Mitchell Armstrong <mra0211@gmail.com>
2022-11-13 23:22:37 -0500

    test commit

--- public/js/getmoviedata.js
+++ public/js/getmoviedata.js
@@ -1,22 +1,21 @@
-// let apiUrl = `https://api.themoviedb.org/3/movie/${postMovieId}?api_key=6bc85f8dbf1308d71b9a884c52f062a1&language=en-US`;
+let apiUrl = `https://api.themoviedb.org/3/movie/${postMovieId}?api_key=${process.env.API_KEY}&language=en-US`;
 
-// fetch(apiUrl)
-//   .then(function (response) {
-//     response.json().then(function (data) {
-//       console.log(data);
-//       showMovieInfo(data);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+fetch(apiUrl)
+  .then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      showMovieInfo(data);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// function showMovieInfo(data) {
-//   console.log("MOVIES!!!!!!!!!!");
-//   const moviePoster = document.querySelector(".movieposter");
-//   document.querySelector(".movietitle").textContent = data.original_title;
-//   document.querySelector(".header-movie-title").textContent =
-//     data.original_title;
-//   moviePoster.innerHTML = `<img class="movieposterimg" src="https://image.tmdb.org/t/p/w185${data.poster_path}" />`;
-//   document.querySelector(".moviedescription").textContent = data.overview;
-// }
+function showMovieInfo(data) {
+  const moviePoster = document.querySelector(".movieposter");
+  document.querySelector(".movietitle").textContent = data.original_title;
+  document.querySelector(".header-movie-title").textContent =
+    data.original_title;
+  moviePoster.innerHTML = `<img class="movieposterimg" src="https://image.tmdb.org/t/p/w185${data.poster_path}" />`;
+  document.querySelector(".moviedescription").textContent = data.overview;
+}
